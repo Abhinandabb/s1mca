f5855a93b64b5250be08c6e7f362e2cb5aa81def
Abhinandabb <150114104+Abhinandabb@users.noreply.github.com>
2024-01-22 18:40:58 +0530

    Add files via upload

---
+++ python/class.py
@@ -0,0 +1,26 @@
+class student:
+    def__init__(self,n,r,c,t)
+    self.name=n
+    self.reg_no=r
+    self.course=c
+    self.totalmark=t
+
+      
+def display(self):
+     print(self.name)
+     print(self.reg_no)
+     print(self.corse)
+
+if self.totalmark>=50 :
+        print("pass")
+else:
+        print("fail")
+
+n=int(input("enter the name"))
+n=int(input("enter the reg_no"))
+n=int(input("enter the course"))
+n=int(input("enter the totalmark"))
+
+obj_stud=student(n,r,c,t)
+obj.student_display()
+        
